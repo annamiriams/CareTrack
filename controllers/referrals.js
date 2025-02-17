308261102c33b877fa207e94681be29823f7ee5f
Anna Steckel <annaextrasteckel@gmail.com>
2025-02-16 19:00:53 -0800

    index.ejs and new.ejs barebones

--- controllers/referrals.js
+++ controllers/referrals.js
@@ -3,18 +3,35 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+// /users/:userId/referrals
+
 // INDEX
 router.get('/', async (req, res) => {
-    res.send('test');
+    const currentUser = await User.findById(req.session.user._id);
+    res.render('referrals/index.ejs', { referral: currentUser.referral });
 });
 
 // NEW
+router.get('/new', async (req, res) => {
+    res.render('referrals/new.ejs');
+});
 
 // DELETE
 
 // UPDATE
 
 // CREATE
+router.get('/new', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.referral.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/referrals`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 // EDIT
 
