21172d1c152e0d7b422f7e84c6bdc1b2f2ba793e
Anna Steckel <annaextrasteckel@gmail.com>
2025-02-23 16:12:35 -0800

    updates made to users ejs pages

--- controllers/users.js
+++ controllers/users.js
@@ -3,17 +3,29 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-// INDEX
+// INDEX (users.ejs)
 router.get('/', async (req, res) => {
     try {
         const users = await User.find();
-        res.render('users/index.ejs', { users: users });
+        res.render('users/users.ejs', { users: users });
     } catch (error) {
         console.log(error);
         res.redirect('/');
     }
 });
 
-// SHOW
+// SHOW (one user's referrals)
+router.get('/:userId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.params.userId);
+        res.render('users/index.ejs', { user: currentUser });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// SHOW (referral details)
+
 
 module.exports = router;
